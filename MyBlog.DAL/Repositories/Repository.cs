0c8e187b9c195b9c4585787f1d03b67ddcb7abf1
Berk Doğramacı <berkdogramaci@gmail.com>
2019-09-11 14:20:55 +0300

    DAL Çalışma

---
+++ MyBlog.DAL/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MyBlog.DAL.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly DbContext _dbcontext;
+        protected readonly DbSet<TEntity> _dbset;
+
+        public Repository(DbContext dbContext)
+        {
+            _dbcontext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _dbset = dbContext.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            _dbset.Add(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
